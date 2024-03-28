8f73d48c21fc0c05690c6c29e91a1939ba2c2c77
MARK CABREROS <markcabreros001@gmail.com>
2024-03-28 15:04:22 +0800

    login interface update :)

--- login.php
+++ login.php
@@ -1,34 +1,34 @@
-<?php 
+<?php
 include "includes/navbar.php";
 
 session_start();
 
-if(isset($_SESSION['logged_in'])){
+if (isset($_SESSION['logged_in'])) {
     header('Location: home.php');
-  }
+}
 
 ?>
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-3">
-            
+
         </div>
         <div class="col-md-6 ">
             <div class="panel panel-info">
                 <div class="panel-heading">LOGIN</div>
                 <div class="panel-body">
                     <form action="login_submit.php" method="POST">
-                        <div class="form-group">
-                            <label>USERNAME</label>
-                            <input type="text" name="uname" class="form-control" />
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="inputGroup-sizing-default">Username</span>
+                            <input type="text" class="form-control" name="uname" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
                         </div>
 
-                        <div class="form-group">
-                            <label>PASSWORD</label>
-                            <input type="password" name="pword" class="form-control"/>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="inputGroup-sizing-default">Password</span>
+                            <input type="text" class="form-control" name="pword" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
                         </div>
 
-                        <input type="submit" name="btnsub" class="btn btn-info"/>
+                        <input type="submit" name="btnsub" class="btn btn-info" />
 
                         <a href="signup.php">Don't have account yet? Register here</a>
                     </form>
@@ -36,12 +36,11 @@ if(isset($_SESSION['logged_in'])){
             </div>
         </div>
         <div class="col-md-3 ">
-            
+
         </div>
     </div>
-    
+
 </div>
 <?php include "includes/footer.php";
 
 ?>
-
